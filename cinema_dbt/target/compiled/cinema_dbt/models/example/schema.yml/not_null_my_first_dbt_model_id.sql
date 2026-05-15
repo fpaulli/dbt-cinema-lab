5d9e5a9afc62594327b50657ec5a866f9c1d1379
fpaulli <frizen_p_o@my.mgimo.ru>
2026-05-15 23:16:31 +0300

    Add files via upload

---
+++ cinema_dbt/target/compiled/cinema_dbt/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from "postgres"."cinema_dbt"."my_first_dbt_model"
+where id is null
+
+
---
+++ cinema_dbt/target/compiled/cinema_dbt/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from "postgres"."cinema_dbt"."my_second_dbt_model"
+where id is null
+
+
